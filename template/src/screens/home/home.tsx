d1d6dd8ea0a9a17b172a6e0de9588f2a61475884
Parth Patel <pspatel1068@gmail.com>
2022-03-10 15:15:39 +0530

    fixed absolute imports

---
+++ template/src/screens/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Button } from '@/components';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Button title={'Hello'} onPress={() => {}} />
+    </View>
+  );
+};
