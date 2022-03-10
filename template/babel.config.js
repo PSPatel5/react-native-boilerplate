299e0cc089c94691e3a823136a8c99915c243f62
Parth Patel <pspatel1068@gmail.com>
2022-03-10 13:49:32 +0530

    theme and absolute imports added

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@/assets/*': './src/assets/*',
+          '@/components/*': './src/components/*',
+          '@/constants/*': './src/constants/*',
+          '@/navigation/*': './src/navigation/*',
+          '@/redux/*': './src/redux/*',
+          '@/screens/*': './src/screens/*',
+          '@/services/*': './src/services/*',
+          '@/themes/*': './src/themes/*',
+          '@/utils/*': './src/utils/*',
+        },
+      },
+    ],
+  ],
 };
---
+++ template/src/themes/images.ts
@@ -0,0 +1,3 @@
+export const Images = {
+  // BoilerPlate: require('assets/Images/react-icon.png'),
+};
